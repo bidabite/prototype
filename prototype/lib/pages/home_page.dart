dc3d22250fe5f5d09f22327f7afeb8bb9d91abbe
SmurfNiZed <zipacaldo@up.edu.ph>
2023-07-01 01:40:53 +0800

    feat: Sign Out

--- prototype/lib/pages/home_page.dart
+++ prototype/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,10 +10,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // sign user out
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home")),
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [
+          // sign out button
+          IconButton(
+            onPressed: signOut,
+            icon: Icon(Icons.logout)
+          ),
+        ],
+      ),
     );
   }
 }
