66c94910fdcf15e2b5081be48878b96dbaf790ad
SmurfNiZed <zipacaldo@up.edu.ph>
2023-07-02 02:25:58 +0800

    feat: floating action button for make post

--- prototype/lib/pages/home_page.dart
+++ prototype/lib/pages/home_page.dart
@@ -1,12 +1,16 @@
+import 'dart:ffi';
+
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:prototype/components/drawer.dart';
 import 'package:prototype/helper/helper_methods.dart';
 import 'package:prototype/pages/profile_page.dart';
 
 import '../components/announcement.dart';
+import '../components/tag.dart';
+import '../components/text_field.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +20,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isDemand = true;
 
   // user
   final currentUser = FirebaseAuth.instance.currentUser!;
@@ -48,6 +53,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void toggleDemand() {
+    setState(() {
+      isDemand = !isDemand;
+    });
+  }
+
   // navigate to profile page
   void goToProfilePage() {
     // go to profile page
@@ -61,7 +72,86 @@ class _HomePageState extends State<HomePage> {
     TextEditingController _searchController = TextEditingController();
 
     return Scaffold(
-      // Nav Bar
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          AwesomeDialog(
+            context: context,
+            dialogType: DialogType.noHeader,
+            animType: AnimType.scale,
+            btnCancelOnPress: () {
+              setState(() {
+                textController.clear();
+              });
+            },
+            btnOkOnPress: () {
+              postMessage();
+            },
+            body: Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10, bottom: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Make an announcement",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const MyTag(name: "Cleaning", color: Colors.blue),
+                          const SizedBox(width: 5),
+                          Container(
+                            width: 25,
+                            height: 25,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.grey[300]!,
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.grey[600]!,
+                              size: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            isDemand = !isDemand;
+                          });
+                        },
+                        child: MyTag(name: isDemand?"Demand":"Supply", color: isDemand?Colors.red:Colors.green)
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: MyTextField(
+                          controller: textController,
+                          hintText: "Write an announcement...",
+                          obscureText: false,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ).show();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color(0xFF0052CC),
+        mini: true
+      ),
       backgroundColor: Colors.white,
       body: Stack(
         children: [
@@ -74,11 +164,11 @@ class _HomePageState extends State<HomePage> {
             child: Center(
               child: Column(
                 children: [
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Padding(
-                        padding: const EdgeInsets.only(left: 20.0, top: 25),
+                        padding: EdgeInsets.only(left: 20.0, top: 25),
                         child: Text(
                           "CONNECT",
                           style: TextStyle(
@@ -93,33 +183,76 @@ class _HomePageState extends State<HomePage> {
 
                   Padding(
                     padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 20),
-                    child: TextField(
-                      controller: _searchController,
-                      obscureText: false,
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 15, top: 10, right: 10, bottom: 10),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF0052CC)),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  offset: const Offset(0, 3),
+                                  blurRadius: 0,
+                                ),
+                              ],
                               borderRadius: BorderRadius.circular(35)
+                            ),
+                            child: TextField(
+                              controller: _searchController,
+                              obscureText: false,
+                              decoration: InputDecoration(
+                                contentPadding: const EdgeInsets.only(left: 15, top: 10, right: 10, bottom: 10),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey[300]!),
+                                  borderRadius: BorderRadius.circular(35),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey[300]!),
+                                  borderRadius: BorderRadius.circular(35),
+                                ),
+                                fillColor: Colors.white,
+                                filled: true,
+                                hintText: "Search",
+                                hintStyle: TextStyle(color: Colors.grey[600]!),
+                                suffixIcon: const Icon(
+                                  Icons.search,
+                                  color: Color(0xFF0052CC),
+                                )
+                              ),
+                            ),
                           ),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF0052CC)),
-                              borderRadius: BorderRadius.circular(35)
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                offset: Offset(0, 3),
+                                blurRadius: 0,
+                              ),
+                            ],
+                            border: Border.all(
+                              color: Colors.grey[300]!
+                            )
                           ),
-                          fillColor: Colors.white,
-                          filled: true,
-                          hintText: "Search",
-                          suffixIcon: Icon(Icons.search),
-                          suffixIconColor: Color(0xFF0052CC),
-                          hintStyle: TextStyle(color: Colors.grey[600]!)
-                      ),
+                          child: const Icon(
+                            Icons.filter_list,
+                            color: Color(0xFF0052CC),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
 
                   // feed
                   Expanded(
                     child: StreamBuilder(
-                      stream: FirebaseFirestore.instance.collection("User Posts").orderBy("Timestamp", descending: false).snapshots(),
+                      stream: FirebaseFirestore.instance.collection("User Posts").orderBy("Timestamp", descending: true).snapshots(),
                       builder: (context, snapshot) {
                         if (snapshot.hasData) {
                           return ListView.builder(
@@ -143,25 +276,7 @@ class _HomePageState extends State<HomePage> {
                       },
                     ),
                   ),
-
-                  // // post announcement
-                  // Padding(
-                  //   padding: const EdgeInsets.all(25.0),
-                  //   child: Row(
-                  //     children: [
-                  //       Expanded(
-                  //         child: MyTextField(
-                  //           controller: textController,
-                  //           hintText: "Write an announcement...",
-                  //           obscureText: false,
-                  //         ),
-                  //       ),
-                  //
-                  //       // post button
-                  //       IconButton(onPressed: postMessage, icon: const Icon(Icons.arrow_circle_up))
-                  //     ],
-                  //   ),
-                  // ),
+                  const SizedBox(height: 20)
                 ],
               ),
             ),
